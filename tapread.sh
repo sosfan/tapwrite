b5bf7352c5f72c29bdfc0304ac2a7feb0121d165
sosfan <sos.sempai@gmail.com>
2015-08-17 18:40:28 -0500

    Update tapread.sh

--- tapread.sh
+++ tapread.sh
@@ -5,13 +5,13 @@ tmp="/tmp/data.tmp"
 echo "Escuchando datos..."
 echo "Use Ctrl + C para detener la operación."
 #Use ésta opción para escuchar la señal análoga mediante el auxiliar.
-minimodem --rx $baudrate -q -a -8 > $tmp
+#minimodem --rx $baudrate -q -a -8 > $tmp
 #Use ésta opción para escuchar mediante el monitor del sistema.
-#minimodem --rx-one $baudrate -q -a -8 > $tmp 
+minimodem --rx-one $baudrate -q -a -8 > $tmp 
 if [[ $# -eq 0 ]];then
 	namefile=`cat $tmp | cut -d "@" -f 2 -s`
-	cat $tmp | cut -d "," -f 1 | base64 -d > "$namefile"
+	cat $tmp | cut -d "," -f 1 | base64 -d -i > "$namefile"
 else
-	cat $tmp | cut -d "," -f 1 | base64 -d > "$1"
+	cat $tmp | cut -d "," -f 1 | base64 -d -i > "$1"
 fi
 rm $tmp
