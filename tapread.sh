efa76133bc265c707b2edb9b01280b50054aa550
sosfan <sos.sempai@gmail.com>
2016-02-02 05:19:16 -0600

    Update tapread.sh

--- tapread.sh
+++ tapread.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-#Uso tapread nombre_salida
-baudrate=4000
+#Uso: taperead [Baudios]
+baudrate=$1
 tmp="/tmp/data.tmp"
+if [[ $1 -eq 0 ]];then
+	echo "Uso: taperead [Baudios]"
+	exit 0
+fi
 echo "Escuchando datos..."
 echo "Use Ctrl + C para detener la operación."
-#Use ésta opción para escuchar la señal análoga mediante el auxiliar.
-#minimodem --rx $baudrate -q -a -8 > $tmp
-#Use ésta opción para escuchar mediante el monitor del sistema.
-minimodem --rx-one $baudrate -q -a -8 > $tmp 
-if [[ $# -eq 0 ]];then
+minimodem --rx-one $baudrate -q > $tmp 
+#if [[ $2 -eq 0 ]];then
 	namefile=`cat $tmp | cut -d "@" -f 2 -s`
-	cat $tmp | cut -d "," -f 1 | base64 -d -i > "$namefile"
-else
-	cat $tmp | cut -d "," -f 1 | base64 -d -i > "$1"
-fi
+	cat $tmp | sed '1d' | sed '$d' | base64 -d > "$namefile"
+	if [[ $namefile = "comp.tar.lzma" ]];then
+		tar --lzma -xvf $namefile
+		rm $namefile
+	fi
+#else
+#	cat $tmp | cut -d "," -f 1 | base64 -d > "$2"
+#fi
 rm $tmp
